d3eb343c5239616dbabffe0c89a2f567710fb8a6
chay123-crypto <chaithanyiap@gmail.com>
2025-11-30 15:46:25 +0530

    Milestone 1: Role based access

--- app.py
+++ app.py
@@ -115,7 +115,185 @@ with app.app_context():
         admins=Admin(email_id=email,password=password,admin_name='ADMIN123')
         db.session.add(admins)
         db.session.commit()
+@app.route("/")
+def initial():
+    return render_template("signin.html")
+            
+@app.route("/sign_in",methods=['POST','GET'])
+def sign_in():
+    if request.method=='POST':
+        email=request.form['mail']
+        password=request.form['password']
+        role=request.form['role']
 
+        pat_blacklist=PatientBlacklist.query.all()
+        pat_black_ids=[pat.patient_id for pat in pat_blacklist]
+        doc_blacklist=DoctorBlacklist.query.all()
+        doc_black_ids=[doc.doctor_id for doc in doc_blacklist]
+
+        if role.lower()=='patient':
+            person=Patient.query.filter_by(email=email,password=password).first()
+            if person:
+                if person.patient_id in pat_black_ids:
+                    flash("You are blacklisted! You can't login","danger")
+                    return redirect("/sign_in")
+                else:
+                    session['role']='patient'
+                    session['id']=person.patient_id
+                    return redirect(url_for('patient',patient_id=person.patient_id))
+            else:
+                flash("No patient available please check the username or password","danger")
+                return redirect("/sign_in")
+        
+        elif role.lower()=='doctor':
+            person=Doctor.query.filter_by(email=email,password=password).first()
+            if person:
+                if person.doc_id in doc_black_ids:
+                    flash("You are blacklisted! You can't login","danger")
+                    return redirect("/sign_in")
+                else:
+                    session['role']='doctor'
+                    session['id']=person.doc_id
+                    return redirect(url_for('doctors',doctor_id=person.doc_id))
+            else:
+                flash("No Doctor available please check the username or password","danger")
+                return redirect("/sign_in")
+            
+        elif role.lower()=='admin':
+            person=Admin.query.filter_by(email_id=email,password=password).first()
+            if person:
+                session['role']='admin'
+                session['id']=person.admin_id
+                return redirect(url_for('admin',admin_id=person.admin_id))
+            else:
+                flash("No admin available please check the username or password","danger")
+                return redirect("/sign_in")
+        else:
+            flash("Invalid Login! Sign Up if you dont have an account","danger")
+
+    return render_template("signin.html")
+@app.route("/patient_signup",methods=['POST','GET'])
+def patient_signup():
+    if request.method=='POST':
+        name=request.form['name']
+        age=int(request.form['age'])
+        phone=request.form['phoneno']
+        gender=request.form['gender']
+        email=request.form['mail']
+        password=request.form['password']
+        height=request.form['height']
+        weight=request.form['weight']
+        city=request.form['city']
+        address=request.form['address']
+        state=request.form['state']
+        dob=request.form['dob']
+        
+        password_pattern=r"^(?=.*[A-Za-z0-9])[A-Za-z0-9@]{8,}$"
+        if not re.fullmatch(password_pattern,password):
+            flash("Choose another password! Min 8 characters","warning")
+            return render_template("pat_signup.html")
+
+        phone_pattern=r"^[6789][0-9]{9}$"
+        if not re.fullmatch(phone_pattern,phone):
+            flash("Enter valid phone number!" ,"warning")
+            return render_template("pat_signup.html")
+
+        name_pattern=r"^[a-zA-Z ]+$"
+        if not re.fullmatch(name_pattern,name):
+            flash("Enter valid name!" ,"warning")
+            return render_template("pat_signup.html")
+        if Patient.query.filter_by(name=name,
+            age=age,
+            phone=phone,
+            gender=gender,
+            email=email,
+            height=height,
+            weight=weight,
+            city=city,
+            state=state,
+            dob=dob).first():
+            flash("You already have an account! Sign In","danger")
+            return redirect("/sign_in")
+        dob=datetime.strptime(dob,"%Y-%m-%d").date()
+        pat=Patient(
+            name=name,
+            age=age,
+            phone=phone,
+            gender=gender,
+            email=email,
+            password=password,
+            height=height,
+            weight=weight,
+            city=city,
+            state=state,
+            address=address,
+            dob=dob
+        )
+        db.session.add(pat)
+        db.session.commit()
+        flash("Successfully registered yourself","success")
+        return render_template("signin.html")
+    
+    return render_template("pat_signup.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template("signin.html")
+
+class DoctorResource(Resource):
+    def get(self,doctor_id=None):
+         if 'add' in request.path:
+            if session.get('role')=='admin':
+                html=render_template('doc_signup.html')
+                return make_response(html,200)
+            else:
+                return redirect("/sign_in")
+            
+    def post(self,doctor_id=None):
+        if session.get('role')=='admin':
+            name=request.form['name']
+            speciality=request.form['speciality']
+            phone=request.form['phoneno']
+            email=request.form['mail']
+            department=request.form['department']
+            password=request.form['password']
+            experience=int(request.form['experience'])
+            comment=request.form['comments']
+            
+            departments=Department.query.filter_by(dept_Name=department).first()
+            if departments:
+                dept=departments.dept_id
+
+                doc=Doctor(
+                    name=name,
+                    speciality=speciality,
+                    phone=phone,
+                    email=email,
+                    dept_id=dept,
+                    password=password,
+                    experience=experience,
+                    comments=comment
+                )
+                if Doctor.query.filter_by(name=name,
+                    speciality=speciality,
+                    phone=phone,
+                    email=email,
+                    dept_id=dept,
+                    password=password,
+                    experience=experience,
+                    comments=comment).first():
+                    flash("already added doctor","primary")
+                db.session.add(doc)
+                db.session.commit()
+                flash("Successfully registered doctor","success")
+                return redirect(url_for("admin"))
+            elif not departments:
+                flash("No department! Add department first","warning")
+                return redirect(url_for("admin"))
+        else:
+            return redirect("/sign_in")
+              
 class DepartmentResource(Resource):
     def get(self,dept_name=None):
         if 'add' in request.path:
@@ -150,6 +328,7 @@ class DepartmentResource(Resource):
         return redirect('/departments')
     
 api.add_resource(DepartmentResource,"/departments","/departments/add","/departments/<string:dept_name>")
+api.add_resource(DoctorResource,"/add_doctors",endpoint='add_doctors')
 
 if __name__=="__main__":
     app.run(debug=True)
